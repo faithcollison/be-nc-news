aec0dae4334f7cf8b4f703b85bebde8aa003bc20
Faith Collison <faithcollison@yahoo.co.uk>
2023-11-20 11:58:43 +0000

    create endpoint for /api/topics, task2

---
+++ controllers/topics.controller.js
@@ -0,0 +1,10 @@
+const {selectAllTopics} = require("../models/topics.models")
+
+exports.getTopics = (req, res, next) => {
+    selectAllTopics().then((topics) => {
+        res.status(200).send({topics})
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+}
