ed0b5879073f60b7d4243a48ea2b276c42bc0054
Faith Collison <faithcollison@yahoo.co.uk>
2023-11-22 09:24:20 +0000

    add endpoint delete /api/comments/:comment_id, update /api endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -49,3 +49,15 @@ exports.selectArticleComment = (article_id) => {
         return result.rows
     })
 }
+
+exports.deleteCommentById = (comment_id) => {
+    return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id])
+    .then((result)=> {
+        if(result.rows.length === 0){
+            return Promise.reject({
+                status: 404,
+                msg: "Comment does not exist"
+            })
+        }
+    }) 
+}
