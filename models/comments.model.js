653bf0325f8762335b3a126897ea1546dc6524b0
Faith Collison <faithcollison@yahoo.co.uk>
2023-11-23 15:33:40 +0000

    create endpoint PATCH /api/comments/:comment_id to update comment votes, udpate /api endpoints

--- models/comments.model.js
+++ models/comments.model.js
@@ -27,3 +27,16 @@ exports.deleteCommentById = (comment_id) => {
         }
     }) 
 }
+
+exports.changeComment = (comment_id, inc_votes) => {
+    return db.query(`UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`, [inc_votes, comment_id])
+    .then((result) => {
+        if(result.rows.length===0){
+            return Promise.reject({
+                status: 404,
+                msg: 'Comment not found'
+            })
+        }
+        return result.rows[0]
+    })
+}
--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,8 +1,11 @@
 const commentsRouter = require("express").Router();
-const {deleteComment} = require("../controllers/comments.controller")
+const { updateArticle } = require("../controllers/articles.controller");
+const {deleteComment, updateComment} = require("../controllers/comments.controller")
 
 commentsRouter
 .route("/:comment_id")
 .delete(deleteComment)
+.patch(updateComment)
+
 
 module.exports = commentsRouter
