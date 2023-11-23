2f1c3f3ab6937439c6e6cbafc3dd02cc7c11dfd4
Faith Collison <faithcollison@yahoo.co.uk>
2023-11-23 12:17:17 +0000

    add sorting queries to GET api/articles endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,12 +1,28 @@
 const db = require("../db/connection.js")
 
-exports.selectArticles = (topicQuery) => {
+exports.selectArticles = (topicQuery, sort_by, order) => {
     const queryValues = []
-    let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id `
+    const validSortBy = ["article_id", "title", "topic", "author", "body", "created_by", "votes"]
+    const validOrder = ['asc', 'desc']
     
+    if (sort_by && ! validSortBy.includes(sort_by)) {
+        return Promise.reject({ status: 400, msg: 'Bad request' });
+    }
+    else if (order && !validOrder.includes(order)) {
+        return Promise.reject({ status: 400, msg: 'Bad request' });
+    }
+    
+    let queryStr = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id `
+
     if(topicQuery){
         queryValues.push(topicQuery)
         queryStr += `WHERE articles.topic = $1 GROUP BY articles.article_id ORDER BY created_at DESC;`
+    }
+    else if(sort_by && !order){
+        queryStr += `GROUP BY articles.article_id ORDER BY articles.${sort_by} DESC`
+    }
+    else if(sort_by && order){
+        queryStr += `GROUP BY articles.article_id ORDER BY articles.${sort_by} ${order}`
     }
     else {
         queryStr += `GROUP BY articles.article_id ORDER BY created_at DESC;`
