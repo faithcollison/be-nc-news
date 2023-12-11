78ea0e71a755990647528e6333f803f14d0e79d4
Faith Collison <faithcollison@yahoo.co.uk>
2023-12-11 10:09:05 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// const {getTopics, getEndpoints} = require("./controllers/topics.controller")
-// const {getArticles, getArticle, updateArticle } = require("./controllers/articles.controller")
-// const { getUsers } = require('./controllers/users.controller');
-// const {getArticleComment, postComment, deleteComment} = require("./controllers/comments.controller")
 const express = require('express');
 const app = express();
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./errors');
 const apiRouter = require("./routes/api-router.js")
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
@@ -16,15 +14,3 @@ app.use(handleServerErrors)
 
 module.exports = app
 
-// app.get("/api/topics", getTopics)
-// app.get("/api/articles/:article_id", getArticle)
-// app.get("/api", getEndpoints)
-// app.get("/api/articles", getArticles)
-// app.get("/api/articles/:article_id/comments", getArticleComment)
-// app.get("/api/users", getUsers)
-
-// app.patch("/api/articles/:article_id", updateArticle)
-
-// app.post("/api/articles/:article_id/comments", postComment)
-
-// app.delete("/api/comments/:comment_id", deleteComment)
